2c996dbf9004e9551023163c25f851a1c0638286
lobaevsl <lobaevlva@gmail.com>
2023-09-01 00:07:51 +0300

    update formula.js

--- formula.js
+++ formula.js
@@ -1,23 +1,43 @@
 const weightPrice = 10;
 const sizePrice = 2000;
+const wayPrice = 200;
+
+const minWeight = 10;
+const minSize = 0.01;
+const minWay = 10;
 
 inputWeight = document.getElementById("weight");
 inputSize = document.getElementById("size");
+inputWay = document.getElementById("way");
 inputOut = document.getElementById("out");
 comment = document.getElementById("comment");
+
+initialize();
+
+function initialize(){
+    inputWeight.value = minWeight;
+    inputSize.value = minSize;
+    inputWay.value = minWay;
+    setOut();
+}
 
 function setOut(){
     checkValue(inputWeight);
     checkValue(inputSize);
+    checkValue(inputWay);
     let weight = inputWeight.value;
     let size = inputSize.value;
-    let _w = weight * weightPrice;
+    let way = inputWay.value;
+    let _we = weight * weightPrice;
     let _s = size * sizePrice;
-    inputOut.value = Math.max(_w, _s);
+    let _wa = way * wayPrice;
+    inputOut.value = Math.max(_we, _s) + _wa;
 }
 
 function lastCheck(input){
     if (input.value === "") input.value = 0;
+    checkAll();
+    setOut();
 }
 
 function checkValue(input){
@@ -31,4 +51,10 @@ function checkValue(input){
         input.value = parseFloat(value);
     }
     return value;
+}
+
+function checkAll(){
+    if (inputWeight.value < minWeight) inputWeight.value = minWeight;
+    if (inputSize.value < minSize) inputSize.value = minSize;
+    if (inputWay.value < minWay) inputWay.value = minWay;
 }
